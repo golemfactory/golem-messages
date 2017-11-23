0bc57a7cc9406ea9115b3db8f8c3a030dcf529c1
Dariusz Rybi <jiivanq@gmail.com>
2017-11-23 16:27:10 +0100

    Release 1.1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import unittest
 
 setup(
     name='Golem Messages',
-    version='1.1.0',
+    version='1.1.1',
     url='https://github.com/golemfactory/golem-messages',
     packages=['golem_messages'],
     install_requires=[
