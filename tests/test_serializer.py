c90519a79e47a08f7a6195e4ad38d2914df496e2
Dariusz Rybi <jiivan@users.noreply.github.com>
2017-12-20 15:33:34 +0100

    Signed message inside a signed message (#51)
    * Signed message inside a signed message
    
    fixes: #50
    
    * review

--- tests/test_serializer.py
+++ tests/test_serializer.py
@@ -1,5 +1,7 @@
+from golem_messages import cryptography
 from golem_messages import message
 from golem_messages import serializer
+from golem_messages import shortcuts
 import unittest
 
 
@@ -30,3 +32,68 @@ class MessageTestCase(unittest.TestCase, EqualityMixIn):
     def test_message_dict(self):
         d = {'m': message.Ping()}
         self.equal_after_processing(d)
+
+    def test_message_sig(self):
+        """Signed message inside a signed message"""
+
+        concent_keys = cryptography.ECCx(None)
+        provider_keys = cryptography.ECCx(None)
+        requestor_keys = cryptography.ECCx(None)
+
+        task_to_compute = message.TaskToCompute()
+        ctd = message.ComputeTaskDef({'task_id': 20, })
+        task_to_compute.compute_task_def = ctd
+
+        # Dump TaskToCompute to make it signed
+        s_task_to_compute = shortcuts.dump(
+            task_to_compute,
+            requestor_keys.raw_privkey,
+            provider_keys.raw_pubkey,
+        )
+
+        # Load TaskToCompute back to its original format
+        task_to_compute = shortcuts.load(
+            s_task_to_compute,
+            provider_keys.raw_privkey,
+            requestor_keys.raw_pubkey,
+        )
+
+        first_sig = task_to_compute.sig
+        first_hash = task_to_compute.get_short_hash()
+
+        # Task TaskToCompute is verified correctly
+        cryptography.ecdsa_verify(
+            requestor_keys.raw_pubkey,
+            task_to_compute.sig,
+            task_to_compute.get_short_hash(),
+        )
+
+        force_report = message.ForceReportComputedTask()
+        force_report.task_to_compute = task_to_compute
+
+        s_force_report = shortcuts.dump(
+            force_report,
+            provider_keys.raw_privkey,
+            concent_keys.raw_pubkey,
+        )
+
+        force_report = shortcuts.load(
+            s_force_report,
+            concent_keys.raw_privkey,
+            provider_keys.raw_pubkey,
+        )
+
+        second_sig = force_report.task_to_compute.sig
+        second_hash = force_report.task_to_compute.get_short_hash()
+
+        self.assertEqual(first_sig, second_sig)
+        self.assertEqual(first_hash, second_hash)
+
+        # Now, attached TaskToCompute should still be verified using
+        # original key. ecdsa_verify will raise InvalidSignature on
+        # failure.
+        cryptography.ecdsa_verify(
+            requestor_keys.raw_pubkey,
+            force_report.task_to_compute.sig,
+            force_report.task_to_compute.get_short_hash(),
+        )
