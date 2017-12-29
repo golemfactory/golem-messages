1af5ee837a7dc5355b26139deb67295165533067
Dariusz Rybi <jiivan@users.noreply.github.com>
2017-12-29 16:14:09 +0100

    Handle empty keys in shortcuts (#63)
    * Handle empty keys in shortcuts
    
    * review

--- golem_messages/shortcuts.py
+++ golem_messages/shortcuts.py
@@ -5,17 +5,30 @@ from . import message
 
 
 def dump(msg, privkey, pubkey):
-    sign = functools.partial(cryptography.ecdsa_sign, privkey)
-    encrypt = functools.partial(cryptography.ECCx.encrypt, raw_pubkey=pubkey)
+    if privkey:
+        sign = functools.partial(cryptography.ecdsa_sign, privkey)
+    else:
+        sign = None
+    if pubkey:
+        encrypt = functools.partial(
+            cryptography.ECCx.encrypt,
+            raw_pubkey=pubkey,
+        )
+    else:
+        encrypt = None
     return msg.serialize(sign_func=sign, encrypt_func=encrypt)
 
 
 def load(data, privkey, pubkey, check_time=True):
     def decrypt(payload):
+        if not privkey:
+            return payload
         ecc = cryptography.ECCx(privkey)
         return ecc.decrypt(payload)
 
     def verify(msg_hash, signature):
+        if not pubkey:
+            return
         cryptography.ecdsa_verify(
             pubkey=pubkey,
             signature=signature,
