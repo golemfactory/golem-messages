d498aa0534523c448b2b3d73ed929e6079ef9230
Dariusz Rybi <jiivanq@gmail.com>
2018-01-12 15:30:22 +0100

    Enable pylint test
    fixes: #81

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
     ],
     tests_require=[
         'pycodestyle',
+        'pylint',
         'freezegun',
     ],
 )
--- tests/test_conformance.py
+++ tests/test_conformance.py
@@ -2,11 +2,14 @@ import golem_messages
 import itertools
 import pathlib
 import pycodestyle
+from pylint import epylint
 import unittest
 
 
 class PEP8TestCase(unittest.TestCase):
-    def test_conformance(self):
+    maxDiff = None
+
+    def test_codestyle(self):
         style = pycodestyle.StyleGuide(ignore=[], max_line_length=80)
         base_path = pathlib.Path(golem_messages.__file__).parent
         tests_path = pathlib.Path(__file__).parent
@@ -18,3 +21,16 @@ class PEP8TestCase(unittest.TestCase):
             result = style.check_files([absolute_path])
             self.assertEqual(result.total_errors, 0,
                              "Found code style errors (and warnings).")
+
+    def test_lint(self):
+        base_path = pathlib.Path(golem_messages.__file__).parent
+        tests_path = pathlib.Path(__file__).parent
+        options = "{tests_dir} {lib_dir} -f parseable".format(
+            lib_dir=base_path,
+            tests_dir=tests_path,
+        )
+        stdout_io, stderr_io = epylint.py_run(options, return_std=True)
+        stdout = stdout_io.read()
+        stderr = stderr_io.read()
+        self.assertEqual(stdout, '')
+        self.assertEqual(stderr, '')
