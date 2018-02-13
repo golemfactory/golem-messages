25c7309e1fc5f558bb23f785d718b2e214e2bf87
shadeofblue <blue@golem.network>
2018-02-13 15:06:16 +0100

    + add list deserializer + add tests

--- tests/message/test_base.py
+++ tests/message/test_base.py
@@ -24,3 +24,48 @@ class MessageTestCase(unittest.TestCase):
                 check_time=False,
             )
         decrypt.assert_called_once_with(mock.ANY)
+
+class VerifySlotChild(base.Message):
+    pass
+
+class VerifySlotParent(base.Message):
+    __slots__ = [
+        'child'
+    ] + base.Message.__slots__
+
+    @base.verify_slot('child', VerifySlotChild)
+    def deserialize_slot(self, key, value):
+        return super().deserialize_slot(key, value)
+
+class VerifySlotListParent(base.Message):
+    __slots__ = [
+        'child_list'
+    ] + base.Message.__slots__
+
+    @base.verify_slot_list('child_list', VerifySlotChild)
+    def deserialize_slot(self, key, value):
+        return super().deserialize_slot(key, value)
+
+class VerifySlotTest(unittest.TestCase):
+    def setUp(self):
+        self.child = VerifySlotChild()
+
+    def test_verify_slot(self):
+        msg = VerifySlotParent(slots=[('child', self.child),])
+        self.assertIsInstance(msg.child, VerifySlotChild)
+
+    def test_verify_slot_not_expected_class(self):
+        with self.assertRaises(exceptions.FieldError):
+            VerifySlotParent(slots=[('child', base.Message()),])
+
+    def test_verify_slot_list(self):
+        msg = VerifySlotListParent(slots=[('child_list', [self.child, ]), ])
+        self.assertIsInstance(msg.child_list[0], VerifySlotChild)
+
+    def test_verify_slot_list_not_list(self):
+        with self.assertRaises(exceptions.FieldError):
+            VerifySlotListParent(slots=[('child_list', self.child), ])
+
+    def test_verify_slot_list_not_expected_class(self):
+        with self.assertRaises(exceptions.FieldError):
+            VerifySlotListParent(slots=[('child_list', [base.Message(), ]), ])
