6a642a296af877e38429df5f8c1bde0e85f62fbf
shadeofblue <blue@golem.network>
2018-03-01 11:27:07 +0100

    [sigh]

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -4,6 +4,7 @@ from unittest import TestCase
 from golem_messages.constants import DEFAULT_UPLOAD_RATE, DOWNLOAD_LEADIN_TIME
 from golem_messages.helpers import maximum_download_time
 
+
 class MaximumDownloadTimeTest(TestCase):
     def test_maximum_download_time(self):
         secs = 100
@@ -12,7 +13,7 @@ class MaximumDownloadTimeTest(TestCase):
         self.assertEqual(expected, maximum_download_time(size))
 
     def test_maximum_download_time_w_rate(self):
-        size = 10240 << 10 # 10M
-        rate = 1024 # KB/s
+        size = 10240 << 10  # 10M
+        rate = 1024  # KB/s
         expected = timedelta(seconds=10) + DOWNLOAD_LEADIN_TIME
         self.assertEqual(expected, maximum_download_time(size, rate))
