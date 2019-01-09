990728dbbb97e716291778ef003a3d314233ab2c
Kuba Mazurek <zakaprov@gmail.com>
2019-01-09 15:47:22 +0100

    Added registered message tests

--- tests/message/test_p2p.py
+++ tests/message/test_p2p.py
@@ -1,11 +1,18 @@
 import unittest
-from golem_messages.factories import p2p
+from golem_messages.factories import p2p as p2p_factories
+from golem_messages.message import p2p
 from tests.message import mixins
 
 
-class WantToStartSessionTest(mixins.SerializationMixin, unittest.TestCase):
-    FACTORY = p2p.WantToStartTaskSessionFactory
+class WantToStartSessionTest(mixins.RegisteredMessageTestMixin,
+                             mixins.SerializationMixin,
+                             unittest.TestCase):
+    FACTORY = p2p_factories.WantToStartTaskSessionFactory
+    MSG_CLASS = p2p.WantToStartTaskSession
 
 
-class SetTaskSessionTest(mixins.SerializationMixin, unittest.TestCase):
-    FACTORY = p2p.SetTaskSessionFactory
+class SetTaskSessionTest(mixins.RegisteredMessageTestMixin,
+                         mixins.SerializationMixin,
+                         unittest.TestCase):
+    FACTORY = p2p_factories.SetTaskSessionFactory
+    MSG_CLASS = p2p.SetTaskSession
