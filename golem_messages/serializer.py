e1536e4a8e2c79f9a18c3c3829a6c291ea44d055
Dariusz Rybi <dariusz@golem.network>
2019-01-23 11:29:05 +0100

    Log more debugging info

--- golem_messages/serializer.py
+++ golem_messages/serializer.py
@@ -1,8 +1,12 @@
 import functools
+import logging
 
 import cbor
 
 from . import exceptions
+
+
+logger = logging.getLogger(__name__)
 
 
 def wrap_error(wrap_with):
@@ -12,7 +16,8 @@ def wrap_error(wrap_with):
             try:
                 return f(*args, **kwargs)
             except Exception as e:
-                raise wrap_with(str(e)) from e
+                logger.debug('Wrapping exception with %s. e=%r', wrap_with, e.__class__, exc_info=True)
+                raise wrap_with("({} ({})".format(e.__class__.__qualname__, e)) from e
         return _curry
     return _inner
 
