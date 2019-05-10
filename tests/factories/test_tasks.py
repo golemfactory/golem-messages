d1070e9f5fbf05e1b7f7acb93c63dfe97c8e9e94
Piotr Chromiec <tworec@golem.network>
2019-05-10 15:59:30 +0200

    test comment

--- tests/factories/test_tasks.py
+++ tests/factories/test_tasks.py
@@ -6,6 +6,8 @@ from golem_messages.utils import pubkey_to_address
 
 class HelpersTest(unittest.TestCase):
 
+    # provider_ethereum_address is not bound to provider_public_key
+    # below binding is only for compatibility with Concent tests
     def test_WTCT_factory_pubkey_bound_to_addr(self):
         wtct = WantToComputeTaskFactory()
         self.assertEquals(wtct.provider_ethereum_address,
